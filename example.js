b3f8dc32481172cd7dd48de13efd373ea4832b13
jasonmzx <68576119+jasonmzx@users.noreply.github.com>
2021-06-05 00:39:25 -0400

    Formatted DRT API & sends back to client

--- example.js
+++ example.js
@@ -4,7 +4,32 @@ let body = { a: 1 };
 const stopId = 2242
 
 function formatJson(json_return){
-console.log(json_return);
+//console.log(json_return);
+let result = json_return.Name + "\n\n"
+
+function TripFormat(item, index){
+    
+
+    //This function calculates the amount of time between the Current Time (c_time) & the time the bus is arriving (a_time)
+    function ArrivalTimeDifference(){
+    //const c_time = [Math.floor(new Date().getTime()/8.64e+7 - 0.166667), new Date().getTime()/8.64e+7 - 0.166667]
+    const c_time = (new Date().getTime()/8.64e+7 - 0.166667 - Math.floor(new Date().getTime()/8.64e+7 - 0.166667))*86400;
+    let a_time = 0;
+    if (item.RealTime > 86400){
+        a_time = item.RealTime-86400;
+    } else {
+        a_time = item.RealTime;
+    };
+    return Math.floor((a_time - c_time)/60)
+    }
+
+    //Here is the formatting for each trip:
+    result = result.concat(`ROUTE ${item.RouteId} - ${item.RealTimeFormatted}\n(arrival in ${ArrivalTimeDifference()} minutes)\n\n`)
+};
+
+json_return.Trips.forEach(TripFormat)
+
+console.log(result)
 };
 
 
--- sms_send.js
+++ sms_send.js
@@ -10,6 +10,36 @@ const body = { a: 1 };
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 
+//This is the Formatter for the API:
+function formatJson(json_return){
+    //console.log(json_return);
+    let result = json_return.Name + "\n\n"
+    
+    function TripFormat(item, index){
+        
+    
+        //This function calculates the amount of time between the Current Time (c_time) & the time the bus is arriving (a_time)
+        function ArrivalTimeDifference(){
+        //const c_time = [Math.floor(new Date().getTime()/8.64e+7 - 0.166667), new Date().getTime()/8.64e+7 - 0.166667]
+        const c_time = (new Date().getTime()/8.64e+7 - 0.166667 - Math.floor(new Date().getTime()/8.64e+7 - 0.166667))*86400;
+        let a_time = 0;
+        if (item.RealTime > 86400){
+            a_time = item.RealTime-86400;
+        } else {
+            a_time = item.RealTime;
+        };
+        return Math.floor((a_time - c_time)/60)
+        }
+    
+        //Here is the formatting for each trip:
+        result = result.concat(`ROUTE ${item.RouteId} - ${item.RealTimeFormatted}\n(arrival in ${ArrivalTimeDifference()} minutes)\n\n`)
+    };
+    
+    json_return.Trips.forEach(TripFormat)
+    
+    return result
+    };
+
 
 
 app.post('/sms', (req,res)=> {
@@ -21,12 +51,11 @@ app.post('/sms', (req,res)=> {
     console.log(`From: ${user_number} , text sent: ${user_input}`);
     //Stop request: If user's input contains '@' at the 0th index of the string & the rest of the string is numeric (Not NaN), then proceed to try and fetch said number from DRT API!
     if (user_input[0] == "@" && isNaN(user_input.slice(1, user_input.length)) == false ){
-    twiml.message("Succesful stop requst command!")
+    //twiml.message("Succesful stop requst command!")
     const stopId = user_input.slice(1, user_input.length)
 
     function messageSend(json_return){
-        console.log(json_return.Name)
-        twiml.message(json_return.Name);
+        twiml.message(formatJson(json_return));
     
         res.writeHead(200, { 'Content-Type': 'text/xml' });
         res.end(twiml.toString());

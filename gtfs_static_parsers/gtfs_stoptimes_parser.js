bd5d8e9d5a7eda42bb0fbe39ec11f301b4129d03
jasonmzx <68576119+jasonmzx@users.noreply.github.com>
2021-07-15 15:34:31 -0400

    GTFS Static parsing + appended to MySQL table

---
+++ gtfs_static_parsers/gtfs_stoptimes_parser.js
@@ -0,0 +1,77 @@
+var fs = require('fs') , stop_times = 'gtfs_static/stop_times.txt', trips = 'gtfs_static/trips.txt';
+const mysql = require('mysql');
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'drt_sms_db',
+    multipleStatements: true
+});
+
+//Connect
+db.connect((err) => {
+    if(err){
+        throw err;
+    }
+    console.log('MySQL is loaded!')
+})
+
+
+
+fs.readFile(trips, 'utf8', function(trip_err,trip_data) {
+    if(trip_err) throw trip_err
+
+    //Remove .slice in the future
+    let trip_lines = trip_data.split('\n');
+    
+    const stop_time_data = fs.readFileSync(stop_times, 'utf8')
+
+    let st_lines = stop_time_data.split('\n');
+    //console.log(trip_lines)
+
+
+    //console.log(st_lines);
+    //Trips loop
+    trip_lines.forEach(t_line => {
+        const t_array = t_line.split(',');
+        //Stop times loop 
+        st_lines.forEach(st_line =>{
+            // console.log(st_lines.indexOf(st_line))
+            const st_array = st_line.split(',');
+            //console.log(t_array[2])
+            if(st_array[0] == t_array[2]){
+            //    const rowPushData = {
+            //        route_id: t_array[0],
+            //        service_id: t_array[1],
+            //        trip_id: t_array[2],
+            //        stop_id: st_array[3].slice(0,st_array[3].length-2),
+            //        arrival_time: st_array[1],
+            //        departure_time: st_array[2]
+            //    }
+               //console.log(rowPushData);
+               console.log(st_lines.indexOf(st_line) + " , " + trip_lines.indexOf(t_line));
+                                //Replace null with "Stop_times"
+            db.query(`INSERT INTO NULL 
+                SET route_id = "${t_array[0]}", 
+                service_id = "${t_array[1]}", 
+                trip_id = "${t_array[2]}",
+                stop_id = "${st_array[3].slice(0,st_array[3].length-2)}",
+                arrival_time = "${st_array[1]}",
+                departure_time = "${st_array[2]}"`, (p_err,p_res) =>{
+                console.log(p_res);
+                if(p_err) throw p_err
+
+                });
+            
+
+            }
+
+        });
+
+
+
+
+    });
+
+});
---
+++ gtfs_static_parsers/txt_parser.js
@@ -0,0 +1,59 @@
+var fs = require('fs') , filename = 'stops.txt';
+const mysql = require('mysql');
+//Create MYSQL connection:
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'drt_sms_db',
+    multipleStatements: true
+});
+
+//Connect
+db.connect((err) => {
+    if(err){
+        throw err;
+    }
+    console.log('MySQL is loaded!')
+})
+
+fs.readFile(filename, 'utf8', function(err, data) {
+    if (err) throw err;
+    console.log('OK: ' + filename);
+    let lines = data.split('\n');
+    lines.forEach(line => {
+        let imps = line.split(',');
+        //0 = StopID, 1 = StopCODE, 2 = Stop_name! , 11 = wheelchair accessiciblity
+
+        //STOP_ID cleaner:
+        if (imps[0].slice(imps[0].length-2,imps[0].length) == ':1'){
+            imps[0] = imps[0].slice(0,imps[0].length-2)
+        };
+        // Wheelchair boolean
+        if(imps[11] == 1) {
+           imps[11] = 1 //TRUE
+        } else {
+            imps[11] = 0  //False
+        }
+
+        //
+        const cleanData = {
+            stop_id : imps[0],
+            stop_name : imps[2],
+            wheelchair_access : imps[11],
+
+        }
+                                    //Replace NULL__ with stops 
+        db.query(`INSERT INTO NULL__  
+                    SET stop_id = '${cleanData.stop_id}', 
+                    stop_name = "${cleanData.stop_name}", 
+                    wheelchair_access = ${cleanData.wheelchair_access} `, (err,res) =>{
+            console.log(cleanData)
+            
+            if(err) throw err
+
+        });
+
+
+    });
+  });

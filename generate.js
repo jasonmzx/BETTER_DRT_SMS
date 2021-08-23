5e957c183200428039a6cca8d7e71d897bc20570
jasonmzx <68576119+jasonmzx@users.noreply.github.com>
2021-08-23 11:41:10 -0400

    Updated generate.js file

--- generate.js
+++ generate.js
@@ -12,12 +12,12 @@ const PATH_TO_STATIC = './mysql_table_generators/STATIC_FILES'
 const SETTINGS = {
     EXEC_STATIC_GEN: true, //Set this to true if you'd like to generate any tables from the STATIC_GEN key below:
     STATIC_GEN: { //Here you can configure which tables will be generated if EXEC_STATIC_GEN is true.
-        static_times: [false, static_times_gen.static_times_generate, PATH_TO_STATIC], 
-        stops: [false, stops_gen.stops_generate, PATH_TO_STATIC ],
-        trips: [false, trips_gen.trips_generate, PATH_TO_STATIC],
+        static_times: [true, static_times_gen.static_times_generate, PATH_TO_STATIC], 
+        stops: [true, stops_gen.stops_generate, PATH_TO_STATIC ],
+        trips: [true, trips_gen.trips_generate, PATH_TO_STATIC],
         routes: [true, routes_gen.routes_generate ,PATH_TO_STATIC] //routes isn't a database table, it's a JSON file
     },
-    EXEC_INIT_GEN: false, //Set this to true if you'd like to generate any tables from the INIT_GEN key below:
+    EXEC_INIT_GEN: true, //Set this to true if you'd like to generate any tables from the INIT_GEN key below:
     INIT_GEN: { //Here you can configure which tables will be generated if EXEC_INIT_GEN is true.
         users: [true, all_table_gen.user_generate ],
         stop_aliases: [true, all_table_gen.stop_aliases_generate],
@@ -28,6 +28,14 @@ const SETTINGS = {
 
 let generate = async (SETTINGS) =>{
 
+if(SETTINGS.EXEC_INIT_GEN == true){
+    for(const table of Object.keys(SETTINGS.INIT_GEN)){
+        if(SETTINGS.INIT_GEN[table][0] == true){
+            await SETTINGS.INIT_GEN[table][1]();   
+            }
+        }
+}
+
 if(SETTINGS.EXEC_STATIC_GEN == true){
     //(Object.keys(SETTINGS.STATIC_GEN)).forEach(async (table) => {
     for(const table of Object.keys(SETTINGS.STATIC_GEN)){
@@ -37,13 +45,6 @@ if(SETTINGS.EXEC_STATIC_GEN == true){
     }
 }
 
-if(SETTINGS.EXEC_INIT_GEN == true){
-    for(const table of Object.keys(SETTINGS.INIT_GEN)){
-        if(SETTINGS.INIT_GEN[table][0] == true){
-            await SETTINGS.INIT_GEN[table][1]();   
-            }
-        }
-}
 
 }
 generate(SETTINGS);

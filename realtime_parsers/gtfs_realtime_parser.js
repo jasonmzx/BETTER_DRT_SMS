b719a0004000f03147ae3baa40dc4bea67bb2f4c
jasonmzx <68576119+jasonmzx@users.noreply.github.com>
2021-08-26 00:22:39 -0400

    Update gtfs_realtime_parser.js

--- realtime_parsers/gtfs_realtime_parser.js
+++ realtime_parsers/gtfs_realtime_parser.js
@@ -84,7 +84,9 @@ let stop_data_format = async (trip_data,route_filter) => {
            + date_lib.format(new Date(trips.first.arrivalTime*1000),'h:mm A' )+ '\n\n' //Time it's coming (HH:MM)
         } else {
             formatted += 'ROUTE '+trip_data[trip_arr[0]].routeId+' > '+trips_query[trip_data[trip_arr[0]].tripId]
-            +'\n* Arrival in '+ Math.round((trip_data[trip_arr[0]].arrivalTime - (Date.now()/1000))/60) +' minute(s) @ ' + date_lib.format(new Date(trip_data[trip_arr[0]].arrivalTime*1000),'h:mm A' ) + '\n\n'
+            // +'\n* Arrival in '+ Math.round((trip_data[trip_arr[0]].arrivalTime - (Date.now()/1000))/60) +' minute(s) @ ' + date_lib.format(new Date(trip_data[trip_arr[0]].arrivalTime*1000),'h:mm A' ) + '\n\n'
+            + ( Math.round((trip_data[trip_arr[0]].arrivalTime - (Date.now()/1000))/60) ? ('\n* Arrival in ' + Math.round((trip_data[trip_arr[0]].arrivalTime - (Date.now()/1000))/60)+' minute(s) @') : 'Bus has arrived. @' )
+            + date_lib.format(new Date(trip_data[trip_arr[0]].arrivalTime*1000),'h:mm A' ) + '\n\n'
         }
     });
 
